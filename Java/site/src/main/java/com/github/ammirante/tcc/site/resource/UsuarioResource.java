2903b6dfa4a9c7b4d130fc135b49c685224377a4
douglas.ammirante <douglas.ammirante@automatizai.com.br>
2020-10-25 18:07:34 -0300

    Recuperação de senha, gerador de string aleatória e envio de e-mail na troca de senha

--- Java/site/src/main/java/com/github/ammirante/tcc/site/resource/UsuarioResource.java
+++ Java/site/src/main/java/com/github/ammirante/tcc/site/resource/UsuarioResource.java
@@ -1,16 +1,20 @@
 package com.github.ammirante.tcc.site.resource;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -21,6 +25,10 @@ import com.github.ammirante.tcc.site.dto.PessoaMapper;
 import com.github.ammirante.tcc.site.entidade.Pessoa;
 import com.github.ammirante.tcc.site.infra.ConstraintViolationResponse;
 import com.github.ammirante.tcc.site.security.TokenService;
+import com.github.ammirante.tcc.site.util.RandomString;
+
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 
 /**
  * UsuarioResource
@@ -36,6 +44,9 @@ public class UsuarioResource {
 	
 	@Inject
 	PessoaMapper pessoaMapper;
+	
+	@Inject
+	Mailer mailer;
 	
 	/**
 	 * @param adicionarPessoaDTO
@@ -65,6 +76,25 @@ public class UsuarioResource {
             throw new WebApplicationException(Response.status(404).entity("Usuário ou senha inválido.").build());
         }
         return tokenService.generateAdminToken(pessoa.email, senha);
+    }
+    
+    @PUT
+    @Path("/recuperar-senha")
+    @Transactional
+    public Response recuperarSenha(@QueryParam("login")String login, @QueryParam("email")String email) {
+    	Pessoa pessoa = Pessoa.find("nomeUsuario", login).firstResult();
+    	
+    	if(pessoa == null || !pessoa.email.equals(email)) {
+    		throw new WebApplicationException(Response.status(404).entity("Usuário ou e-mail não encontrados na base.").build());
+    	}
+    	
+    	RandomString randomString = new RandomString(8, ThreadLocalRandom.current());
+    	String novaSenha = randomString.nextString();
+    	pessoa.senha = novaSenha;
+    	pessoa.persist();
+    	mailer.send(Mail.withText(email, "Nova senha - Blog Open Banking", "Sua nova senha é: " + novaSenha));
+    	
+    	return Response.status(Status.OK).build();
     }
 
 }
