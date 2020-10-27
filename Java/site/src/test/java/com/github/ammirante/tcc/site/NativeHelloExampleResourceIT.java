830a2df1650a2d689a013c1baa03c985f0a660e9
douglas.ammirante <douglas.ammirante@automatizai.com.br>
2020-10-26 21:58:18 -0300

    Remoção dos testes

--- Java/site/src/test/java/com/github/ammirante/tcc/site/NativeHelloExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.github.ammirante.tcc.site;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloExampleResourceIT extends HelloExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
