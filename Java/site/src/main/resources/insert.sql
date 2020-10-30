82330bed916f0fb9a9b53cbf30062a85942886bd
douglas.ammirante <douglas.ammirante@automatizai.com.br>
2020-10-29 23:49:56 -0300

    Ajuste no script insert.sql para incluir pessoas na tabela PESSOA, ajuste no application.properties (marretando o ip) e ajuste no Dockerfile.multistage e docker-compose.yml

--- Java/site/src/main/resources/insert.sql
+++ Java/site/src/main/resources/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO public.dominio_situacao VALUES(1, 'Ativo'), (2, 'Inativo');
 INSERT INTO public.dominio_categoria VALUES(2, 'PIX'), (3, 'Educação Financeira'), (4, 'Transformação Digital'), (1, 'Open Banking');
+INSERT INTO public.pessoa VALUES(DEFAULT, '2020-10-28 00:00:00.000', '2020-10-28 00:00:00.000', 'teste@teste.com', 'Nepo', 'nepo', '123456');
+INSERT INTO public.pessoa VALUES(DEFAULT, '2020-10-28 00:00:00.000', '2020-10-28 00:00:00.000', 'douglas.ammirante@automatizai.com.br', 'Douglas Ammirante da Cunha', 'douglas.ammirante', '123456');
